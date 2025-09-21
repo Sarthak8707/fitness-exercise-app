b0872a23c180a33b5f174054b67bf5143f97dffe
sarthak <sarthakssingh49@gmail.com>
2025-09-21 23:34:08 +0530

    Added Footer

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -5,7 +5,7 @@ const ExerciseVideos = ({ videoDetail }) => {
   const demoVideos = videoDetail.contents?.filter(item => item.type === 'video') || [];
   const videosToShow = demoVideos.slice(0, 3);
 
-
+console.log(videosToShow[0]);
 
   return (
     <Box mt="60px">
@@ -13,14 +13,21 @@ const ExerciseVideos = ({ videoDetail }) => {
         Watch these videos for reference
       </Typography>
 
-      <Stack direction="row" flexWrap="wrap" gap="20px">
+      <Stack direction="row" flexWrap="wrap" gap="20px" mt="50px">
         {videosToShow.map((item, index) => (
-          <img
+          <a key={index}
+           className='exercise-video'
+           href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+           target="_blank"
+           rel="noreferrer"
+           >
+            <img
             key={index}
             src={item?.video?.thumbnails[0]?.url}
             alt={item?.video?.title}
             style={{ width: '350px', height: '200px', objectFit: 'cover', borderRadius: '8px' }}
           />
+          </a>
         ))}
       </Stack>
     </Box>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box 
+      component="footer"
+      sx={{ 
+        mt: 6, 
+        py: 3, 
+        textAlign: "center", 
+        bgcolor: "rgba(128,0,0,0.05)", 
+        borderTop: "1px solid #ddd" 
+      }}
+    >
+      <Typography variant="h6" fontWeight="bold" color="darkred">
+        GrindOn
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        Created for Fitness Enthusiasts in 2025
+      </Typography>
+    </Box>
   )
 }
 
